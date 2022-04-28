ac61577424eb1659a371d1b4d89e73320dc96a3f
Flávio Wagner <flavio-wagner@outlook.com>
2022-04-28 14:40:19 -0300

    Estado

---
+++ appnovo/src/main/java/br/com/appnovo/controller/EstadoController.java
@@ -0,0 +1,25 @@
+package br.com.appnovo.controller;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.appnovo.dto.EstadoDTO;
+import br.com.appnovo.repository.EstadoRepository;
+
+@RestController
+public class EstadoController {
+	@Autowired
+	EstadoRepository estadoRepository;
+	
+	@RequestMapping("/estados")
+	public List<EstadoDTO> ListEstados(){
+		return estadoRepository.findAll()
+				               .stream()
+				               .map(x -> new EstadoDTO(x))
+				               .collect(Collectors.toList());
+	}
+}
